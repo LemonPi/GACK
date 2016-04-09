96632151b5bc0a71531cc364d7214c8e9464bb70
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-04-09 13:47:33 -0400

    Show domain-1 vars in displaytoulbar

--- displaytoulbar2.py
+++ displaytoulbar2.py
@@ -1,5 +1,6 @@
 import sys
 varnames = []
+removedvars = {}
 with open("theoutfile.txt", "r") as infile:
 	firstline = True
 	for l in infile:
@@ -11,14 +12,20 @@ with open("theoutfile.txt", "r") as infile:
 		varnames.append(l.strip().split(" ")[0])
 with open("removedvars.txt", "r") as infile:
 	for l in infile:
-		varnames.remove(l.strip().split(" ")[0])
+		thevalues = l.strip().split(" ")
+		removedvars[thevalues[0]] = thevalues[1]
 with open(sys.argv[1], "r") as infile:
 	foundit = False
 	for l in infile:
 		if foundit:
 			parts = l.strip().split(" ")
-			for i in range(len(parts)):
-				print(varnames[i], parts[i])
+			i = 0
+			for varname in varnames:
+				if varname in removedvars:
+					print(varname, removedvars[varname])
+				else:
+					print(varname, parts[i])
+					i += 1
 			break
 		else:
 			if "New solution" in l:
