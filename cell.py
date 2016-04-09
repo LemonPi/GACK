87e5457466c0160d594db1d3dd8284e527b0ea5e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-04-08 20:01:33 -0400

    Test sugar outputs

--- cell.py
+++ cell.py
@@ -1,3 +1,7 @@
+class Variable:
+	def __init__(self, name, domain):
+		self.name = name
+		self.domain = domain
 
 class Cell:
 	"""represent a spacial region with all the plant properties that are allowed there"""
@@ -19,12 +23,23 @@ class Cell:
 
 
 		# mutable Variables of the space that should have their domains pruned
+		self.variables = []
+		self.add_var(Variable("ph", [0, 1, 2]))
+		self.add_var(Variable("moist", [0, 1, 2, 3]))
 
 
 
 	def add_neighbour(self, neighbour):
 		"""shoulder only be done during initialization; neighbour is another cell object"""
 		self.adj.append(neighbour)
+
+	def add_var(self, var):
+		self.variables.append(var)
+	def get_var(self, varname):
+		for var in self.variables:
+			if var.name == varname:
+				return var
+		return var
 
 	def __repr__(self):
 		return "Cell(index={},area={},nat_shading={},view_dist={}, adj_count={})".format(
@@ -54,9 +69,40 @@ def buildcells():
 		for l in infile:
 			a = [int(b) for b in l.strip().split(",")]
 			cell = cells[a[0]]
-			for neighbor in a[1:]:
+			for neighbor in sorted(a[1:]):
 				addneighbour2way(cell, cells[neighbor])
 	# flood fill for view dist
 	# yeah I know this is wrong
 	flood(cells[0], -1)
 	return cells
+
+def writealladjclause(cells, outfile):
+	for cell in cells:
+		if cell.index == 0:
+			continue
+		for var in cell.variables:
+			print("(int cell{}_{} ({}))".format(cell.index, 
+				var.name, " ".join(str(a) for a in var.domain)),
+			file=outfile)
+		for adj in cell.adj:
+			if adj.index > cell.index:
+				continue
+			if adj.index == 0:
+				continue
+			# soil must be similar
+			# 0, 1, 2
+			# so maximum difference must be < 2
+			print("(< (abs (- cell{}_ph cell{}_ph)) 2)".
+				format(cell.index, adj.index), file=outfile)
+			print("(< (abs (- cell{}_moist cell{}_moist)) 2)".
+				format(cell.index, adj.index), file=outfile)
+
+def main():
+	cells = buildcells()
+	cells[3].get_var("ph").domain = [0];
+	cells[3].get_var("moisture").domain = [0];
+	with open("theoutfile.txt", "w") as outfile:
+		writealladjclause(cells, outfile)
+
+if __name__ == "__main__":
+	main()
