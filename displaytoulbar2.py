fc46fe3f77f1be368b6eb661815158624386a9dc
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-04-09 15:07:38 -0400

    Reverse colour domain to prefer purple; fix display

--- displaytoulbar2.py
+++ displaytoulbar2.py
@@ -1,5 +1,6 @@
 import sys
 varnames = []
+vardoms = []
 removedvars = {}
 with open("theoutfile.txt", "r") as infile:
 	firstline = True
@@ -9,7 +10,9 @@ with open("theoutfile.txt", "r") as infile:
 			continue
 		if "(" in l:
 			continue
-		varnames.append(l.strip().split(" ")[0])
+		varline = l.strip().split(" ")
+		varnames.append(varline[0])
+		vardoms.append(varline[1:])
 with open("removedvars.txt", "r") as infile:
 	for l in infile:
 		thevalues = l.strip().split(" ")
@@ -20,11 +23,12 @@ with open(sys.argv[1], "r") as infile:
 		if foundit:
 			parts = l.strip().split(" ")
 			i = 0
-			for varname in varnames:
+			for vari in range(len(varnames)):
+				varname = varnames[vari]
 				if varname in removedvars:
 					print(varname, removedvars[varname])
 				else:
-					print(varname, parts[i])
+					print(varname, vardoms[vari][int(parts[i])])
 					i += 1
 			break
 		else:
