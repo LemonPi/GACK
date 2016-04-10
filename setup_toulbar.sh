5c0f71916bb458c8e3ac5d9af36046e85c8bb8e3
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-04-09 20:08:50 -0400

    Add script to install ToulBar 2 to the current folder

---
+++ setup_toulbar.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Downloads and installs ToulBar 2 (a Max-CSP solver), which is required for multiple parts of this project.
+# Internet access and about 15 MB of disk space is required.
+wget "https://mulcyber.toulouse.inra.fr/frs/download.php/1447/toulbar2.0.9.8.0-Release-x86_64.tar.gz"
+tar xf toulbar2.0.9.8.0-Release-x86_64.tar.gz
